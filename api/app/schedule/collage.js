27993aa459f7a7e6faf68e947ed914bd8b7601e8
jiangpeng <jiangpeng@houbank.com>
2018-10-24 11:47:11 +0800

    修改代码

--- api/app/schedule/collage.js
+++ api/app/schedule/collage.js
@@ -1,6 +1,10 @@
 const Subscription = require('egg').Subscription;
 const nodeFetch = require("node-fetch")
 class autoGetBTC {
+    constructor() {
+        this.otherMap = new Map()
+    }
+
     async fetch(token, url, method = "GET") {
         let res = await nodeFetch(url, {
             method: method,
@@ -21,11 +25,14 @@ class autoGetBTC {
     }
 
     async  getUserData(token, user) {
+        if (!this.otherMap.has(user.userId)) {
+            this.otherMap.set(user.userId, user)
+        }
         let infoData = await this.fetch(token, 'https://walletgateway.gxb.io/miner/steal/' + user.userId + '/mine/list')
         for (let info of infoData) {
             if (info.canSteal) {
                 let result = await this.fetch(token, 'https://walletgateway.gxb.io/miner/steal/' + user.userId + '/mine/' + info.mineId, 'POST')
-                //console.info(`收取【${user.nickName}】的币${result.stealPercent}%`)
+                console.info(`收取【${user.nickName}】的${info.symbol}币${result.stealPercent}%,数量${result.stealAmount}`)
             }
         }
     }
@@ -48,29 +55,68 @@ class autoGetBTC {
         return listData.length
     }
 
+    async getMyInfo(token) { //获取个人信息
+        return await this.fetch(token, 'https://walletgateway.gxb.io/customer/MhioHcR0Enw049t97fv0238248844')
+    }
+
+    async getMyBTC(token) {
+        let data = await this.fetch(token, 'https://walletgateway.gxb.io/miner/MhioHcR0Enw049t97fv0238248844/mine/list/v2')
+        if (data && data.mines) {
+            for (let item of data.mines) {
+                if (Date.now() >= item.validTime){
+                    let oneData = await this.fetch(token, `https://walletgateway.gxb.io/miner/MhioHcR0Enw049t97fv0238248844/mine/${item.id}/v2`)
+                    console.info(`收取自己的币[${item.symbol}]：${oneData.drawAmount}`)
+                }
+            }
+        }
+    }
+
+    async getMyVoucher(token) {
+        let data = await this.fetch(token, 'https://walletgateway.gxb.io/miner/MhioHcR0Enw049t97fv0238248844/mine/cardVoucher/list')
+        if (data && data.mines) {
+            for (let item of data.mines) {
+                try {
+                    await this.fetch(token, `https://walletgateway.gxb.io/miner/mine/cardVoucher/gain/${item.id}`)
+                    console.info(`收取自己的券[${item.appName}]-[${item.title}]：${oneData.amount}${item.unit}`)
+                } catch (e) {
+
+                }
+            }
+        }
+    }
+
     async  getBTC() {
         let canGet = 0
         let count = 1
 
         let jiang = 'TWhpb0hjUjBFbncwNDl0OTdmdjAyMzgyNDg4NDQ6MDU4OXFhQXRiQWhpamNXMWdhUVFhT245ODM5'
 
+        this.getMyInfo(jiang)
+
+        this.getMyBTC(jiang)
+
         do {
             canGet = await this.get(jiang)
-             console.info(`第${count}次可收取人数${canGet}`)
+            console.info(`第${count}次可收取人数${canGet}`)
             count++
         } while (canGet > 0)
-         console.info(`收取偷我的人的币`)
+        console.info(`收取偷我的人的币`)
         await this.getOther(jiang)
+
+        console.info(`收取存储人的币`)
+
+        for (let user of this.otherMap) {
+            await this.getUserData(jiang, user)
+        }
     }
 }
 const autoGet = new autoGetBTC()
 
 
-
 class setCollage extends Subscription {
     static get schedule() {
         return {
-            cron: '0 0 * * * *',//秒 分 时 天 月 周几
+            cron: '0 0 */2 * * *',//秒 分 时 天 月 周几   两个小时执行一次
             type: 'all', // 指定所有的 worker 都需要执行,
             immediate: true //项目启动后执行一次
         };
